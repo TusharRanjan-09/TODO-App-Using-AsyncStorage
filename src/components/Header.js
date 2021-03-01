d39369dc31f534a8df3bc825b45afe56d86ffba3
TusharRanjan-09 <tushar.r@jeanmartin.com>
2021-03-01 17:02:30 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 export default class Header extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <AntDesign name="bars" size={38} color="white" style={{marginLeft:6}}/>
                 <Text style={styles.title}>TODO</Text>
+                <MaterialCommunityIcons name="dots-vertical" size={38} color="white" />
             </View>
         );
     }
@@ -16,11 +20,14 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: '100%',
         height: '7%',
-        justifyContent: 'center'
+        justifyContent: 'space-between',
+        flexDirection: 'row'
     },
     title: {
         color: 'white',
         fontSize:27,
-        fontWeight:'bold'
+        fontWeight:'bold',
+        alignSelf:'center',
+        marginLeft:25
     }
 });
